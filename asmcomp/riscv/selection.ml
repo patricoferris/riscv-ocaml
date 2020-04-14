23b46af6ff0b0f8ed6c24731393d4813a001d1af
patricoferris <patrickferris17@gmail.com>
2020-04-14 15:46:49 +0100

    Dereference the config variable II

--- asmcomp/riscv/selection.ml
+++ asmcomp/riscv/selection.ml
@@ -22,7 +22,7 @@ class selector = object (self)
 
 inherit Selectgen.selector_generic as super
 
-val rvconfig = mk_config Clflags.riscv_arch 
+val rvconfig = mk_config !Clflags.riscv_arch 
 
 method is_immediate n = is_immediate n
 
@@ -91,7 +91,7 @@ method! emit_expr (env:Selectgen.environment) exp =
 method! emit_tail (env:Selectgen.environment) exp = 
   match exp with 
   | (Cifthenelse (Cop(Ccmpi Cne, [a; Cconst_int 1], debug), Cconst_pointer ifso, Cconst_pointer ifnot)) -> 
-    if (ifso = 1) && (ifnot = 3) && ((!rvconfig).iszero) then (
+    if (ifso = 1) && (ifnot = 3) && (rvconfig.iszero) then (
       self#emit_return env (Cifthenelse (Cop(Ccmpi Cne, [a; Cconst_int 1], debug), Cconst_pointer ifso, Cconst_pointer ifnot))    
     ) else (
       super#emit_tail env exp
